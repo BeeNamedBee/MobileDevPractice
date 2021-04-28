fb94d291ec4c628e5ec990ae8b7466e41559b6c0
m0rph3us <m0rph3us@(NULL).(NULL)>
2021-04-28 11:10:13 +0300

    pract4

---
+++ MireaProject/app/src/main/java/ru/mirea/msv/mireaproject/PlayerService.java
@@ -0,0 +1,30 @@
+package ru.mirea.msv.mireaproject;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class PlayerService extends Service {
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate(){
+        mediaPlayer = MediaPlayer.create(this, R.raw.track001);
+        mediaPlayer.setLooping(true);
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        mediaPlayer.start();
+        return START_STICKY;
+    }
+    @Override
+    public void onDestroy() {
+        mediaPlayer.stop();
+    }
+}
